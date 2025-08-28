d1a833d55621cb27484369fd2c8b83fa5da65e6a
Nytokon <dante.konoba@gmail.com>
2025-08-28 10:13:42 -0300

    empiezo a cambiar los on click

--- Software/Front End/Pantalla de Inicio de Sesion/inicio de sesion.js
+++ Software/Front End/Pantalla de Inicio de Sesion/inicio de sesion.js
@@ -1,6 +1,11 @@
+let BotonVolver= document.getElementById("VolverMenu")
 let emailusuario= document.getElementById("escribir.email");
 let contraseña= document.getElementById("escribir.contra");
-const Botoniniciarsesion = document.getElementById("BotonIniciarSesion")
+const Botoniniciarsesion = document.getElementById("BotonIniciarSesion");
+BotonVolver.addEventListener("click", FuncionRegresar);
+function FuncionRegresar (){
+window.location.href = '../Pantalla de inicio no registrados/pantalla de inicio no registrados.html'
+}
 Botoniniciarsesion.addEventListener("click",(click) => {
     alert(emailusuario.value +" "+ contraseña.value);
   })
