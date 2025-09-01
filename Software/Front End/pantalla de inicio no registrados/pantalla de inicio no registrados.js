a0cf5790ef1567ed999c744a808327d867d5e7fb
Nytokon <dante.konoba@gmail.com>
2025-09-01 10:36:42 -0300

    Termino de reemplazar los onclick del inicio no registrados

--- Software/Front End/pantalla de inicio no registrados/pantalla de inicio no registrados.js
+++ Software/Front End/pantalla de inicio no registrados/pantalla de inicio no registrados.js
@@ -1,5 +1,10 @@
 let BotonRegistrarse = document.getElementById("BotonRegistrarse");
+let BotonIniciarSesion = document.getElementById("BotonIniciarSesion");
+function IniciarSesion (){
+    window.location.href = '../Pantalla de Inicio de Sesion/Inicio de sesion.html';
+};
 function Registrarse (){
-window.location.href = '../PantallaRegistro/PantallaRegistro.html'
+    window.location.href = '../PantallaRegistro/PantallaRegistro.html';
 };
 BotonRegistrarse.addEventListener("click",(Registrarse));
+BotonIniciarSesion.addEventListener("click",(IniciarSesion))
