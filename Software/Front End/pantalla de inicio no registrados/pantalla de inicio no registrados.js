216689716edd1ca7b06cf8ff51e513fbf46f80ca
Nytokon <dante.konoba@gmail.com>
2025-09-01 10:17:41 -0300

    Cambio el onclick del boton de registrarse

--- Software/Front End/pantalla de inicio no registrados/pantalla de inicio no registrados.js
+++ Software/Front End/pantalla de inicio no registrados/pantalla de inicio no registrados.js
@@ -1,4 +1,5 @@
-let BotonRegistrarse =
+let BotonRegistrarse = document.getElementById("BotonRegistrarse");
 function Registrarse (){
 window.location.href = '../PantallaRegistro/PantallaRegistro.html'
-}
+};
+BotonRegistrarse.addEventListener("click",(Registrarse));
