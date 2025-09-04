1bf409b5bfd793bc1485bd4e4369b80b6cd074e5
Nytokon <dante.konoba@gmail.com>
2025-09-04 10:38:18 -0300

    creo los botones de pantalla inicio donante le doy funcionalidad a 1

---
+++ Software/Front End/Pantalla de Inicio Donantes/InicioDonantes.js
@@ -0,0 +1,5 @@
+let BotonGestionarPerfil= document.getElementById("BotonGestionPerfil");
+function IrGestionarPerfil(){
+    window.location.href = "../Pantalla de gestion de Perfil/Pantalla gestion perfil.html"
+};
+BotonGestionarPerfil.addEventListener("click", (IrGestionarPerfil));
