49026f86e0ba0d6aa54b9c0f6ea047a35db505fa
Nytokon <dante.konoba@gmail.com>
2025-09-04 11:41:47 -0300

    debugeo el boton de volver al inicio

--- Software/Front End/Pantalla de Inicio Donantes/InicioDonantes.js
+++ Software/Front End/Pantalla de Inicio Donantes/InicioDonantes.js
@@ -3,3 +3,7 @@ function IrGestionarPerfil(){
     window.location.href = "../Pantalla de gestion de Perfil/Pantalla gestion perfil.html"
 };
 BotonGestionarPerfil.addEventListener("click", (IrGestionarPerfil));
+let BotonCerrarSesion = document.getElementById("BotonCerrarSesion");
+function IrCerrarSesion(){
+    
+}
