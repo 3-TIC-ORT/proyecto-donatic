e995965ab117622d6e7384730fb5b436baa1c063
Valen <valencampora@gmail.com>
2025-09-10 12:03:06 -0300

    linkee dos botones con sus respectivaas pantallas

---
+++ Software/Front End/PantallaRegistro/PantallaRegistro.js
@@ -0,0 +1,8 @@
+Botonvolver.addEventListener("click", Funcionvolver)
+function Funcionvolver (){
+    window.location.href = '../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
+}
+BotonRegistrarse.addEventListener("click", FuncionRegistrarse)
+function FuncionRegistrarse (){
+    window.location.href = '../Pantalla de Inicio Donantes/InicioDonantes.html'
+}
