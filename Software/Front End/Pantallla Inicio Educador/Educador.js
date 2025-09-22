955b541c33707927392ce8b8f888acc692cb08f8
Nytokon <dante.konoba@gmail.com>
2025-09-22 10:19:17 -0300

    Creo Pantalla de crear info y la de incio educador

---
+++ Software/Front End/Pantallla Inicio Educador/Educador.js
@@ -0,0 +1,5 @@
+let BotonIrCrearInfo = document.getElementById("BotonIrInfo");
+function IrCrearInfo (){
+    window.location.href='../Pantalla Crear Info/CrearInfo.html'
+}
+BotonIrCrearInfo.addEventListener("click", (IrCrearInfo));
