ca00fadb58219f75325717dc696b89c0e5cef27e
urzurioliver <urzurioliver@gmail.com>
2025-09-25 10:32:29 -0300

    inicio registro

--- Software/Back/registro.js
+++ Software/Back/registro.js
@@ -1,1 +1,4 @@
 import fs from "fs";
+let usuario = {mail: "zzz@gmail.com", contraseña : 2468}
+let usuariojson = JSON.stringify (usuario)
+fs.writeFileSync ("Software/Back/usuarios.json", usuariojson)
