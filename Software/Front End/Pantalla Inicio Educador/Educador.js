fca7874dc6de71f7ea430c47b416b93cc2e0d950
Nytokon <dante.konoba@gmail.com>
2025-09-25 09:59:49 -0300

    boton cerrar sesion de pagina inicio educador

--- Software/Front End/Pantalla Inicio Educador/Educador.js
+++ Software/Front End/Pantalla Inicio Educador/Educador.js
@@ -3,3 +3,8 @@ function IrCrearInfo (){
     window.location.href='../Pantalla Crear Info/CrearInfo.html'
 }
 BotonIrCrearInfo.addEventListener("click", (IrCrearInfo));
+let BotonCerrarLaSesion = document.getElementById("BotonCerrarSesion");
+function CerrarSesion (){
+    window.location.href='../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
+}
+BotonCerrarLaSesion.addEventListener("click", (CerrarSesion));
