23d6155e8cc428f457c421f6f6c420d97056a20e
urzurioliver <urzurioliver@gmail.com>
2025-09-25 10:16:18 -0300

    LOGIN TERMINADO

--- Software/Back/login.js
+++ Software/Back/login.js
@@ -1,18 +1,16 @@
 
 import fs from "fs"; 
 let usuario = fs.readFileSync("usuarios.json", "utf-8")
-let usuarioArray = JSON.parse(usuario).usuarios;
+let usuarioArray = JSON.parse(usuario);
 
 function login (mail, contraseña){
+    let correcto = false;
 for (let i=0; i<usuarioArray.length; i++){
     if (mail===usuarioArray[i].mail && contraseña===usuarioArray[i].contraseña){
-        let correcto = true
-    }
-    else {
-        let correcto = false
-    }
-    console.log (correcto)
-        
+        correcto = true
+    }     
 }
+console.log (correcto)
+
 }
 login("yyy@gmail.com", 5678)
