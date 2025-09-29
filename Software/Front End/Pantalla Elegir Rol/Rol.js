a147fb394e3755c544e3b9d37c7a0707ad31c079
Nytokon <dante.konoba@gmail.com>
2025-09-29 10:01:54 -0300

    Creo la pantalla de elegir rol

---
+++ Software/Front End/Pantalla Elegir Rol/Rol.js
@@ -0,0 +1,10 @@
+let BotonCerrarLaSesion = document.getElementById("ButonCerrarSesion");
+function CerrarSesion (){
+    window.location.href='../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
+}
+BotonCerrarLaSesion.addEventListener("click", (CerrarSesion));
+let BotonEstudiante = document.getElementById("Estudiante");
+function IrEstudiante (){
+    window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
+}
+BotonEstudiante.addEventListener("click", (IrEstudiante));
