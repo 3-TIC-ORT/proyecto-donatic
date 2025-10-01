8cc0aabfba007b1bc21e3e08b98e243c8b0c2871
Nytokon <dante.konoba@gmail.com>
2025-10-01 12:04:26 -0300

    Termino el juego verdadero o falso, despues sigo con el cambio de color o mas indicadores(sacar el alert)

--- Software/Front End/Juego Verdero Falso/VF.js
+++ Software/Front End/Juego Verdero Falso/VF.js
@@ -12,3 +12,57 @@ const elementoPregunta = document.getElementById("pregunta");
 const BtnV = document.getElementById("btnV");
 const BtnF = document.getElementById("btnF");
 const elementoPuntaje = document.getElementById("puntaje");
+function mostrarPregunta() {
+    if (NumeroPregunta < Preguntas.length) {
+
+
+        elementoPregunta.textContent = Preguntas[NumeroPregunta].pregunta;
+    } else {
+        elementoPregunta.textContent = `Tu puntaje final es: ${Puntaje} de ${Preguntas.length}.`;
+       
+
+
+        BtnV.disabled = true;
+        BtnF.disabled = true;
+    }
+    if (elementoPuntaje) {
+        elementoPuntaje.textContent = `Puntaje: ${Puntaje}`;
+    }
+
+
+    
+}
+    if (elementoPuntaje) {
+        elementoPuntaje.textContent = `Puntaje: ${Puntaje}`;
+    } 
+
+function verificarRespuesta(respuestaUsuario) {
+    if (NumeroPregunta >= Preguntas.length) return;
+    const preguntaActual = Preguntas[NumeroPregunta];
+   
+    if (respuestaUsuario === preguntaActual.Respuesta) {
+        alert("¡Correcto!");
+        Indicador.classList.add("correcto");
+        Puntaje++;
+    } else {
+        alert(`¡Incorrecto!`);
+        Indicador.classList.add("incorrecto");
+    }
+   
+    NumeroPregunta++;
+   
+    mostrarPregunta();
+}
+if (BtnV) {
+    BtnV.addEventListener('click', () => {
+        verificarRespuesta(true);
+    });
+}
+
+
+if (BtnF) {
+    BtnF.addEventListener('click', () => {
+        verificarRespuesta(false);
+    });
+}
+mostrarPregunta();
