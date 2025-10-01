f173243a4fe81865f06b245b3f32d43acb947483
Nytokon <dante.konoba@gmail.com>
2025-10-01 10:36:06 -0300

    creo todos  los elementos necesarios y comienzo a diagramar la funcionalidad

--- Software/Front End/Juego Verdero Falso/VF.js
+++ Software/Front End/Juego Verdero Falso/VF.js
@@ -1,16 +1,14 @@
-const Preguntas = {
-    pregunta: "2 + 2 es 4?",
-    Respuesta: true 
-};
-
-let rtaString = prompt(Preguntas.pregunta);
-
-
-let rtaBoolean = rtaString.toLowerCase() === 'true';
+const Preguntas = [
+    { pregunta: "¿2 + 2 es 4?", Respuesta: true, Numero: 1 },
+    { pregunta: "¿2 + 8 es 11?", Respuesta: false, Numero: 2 },
+    { pregunta: "¿2 + 3 es 5?", Respuesta: true, Numero: 3 },
+    { pregunta: "¿2 + 4 es 6?", Respuesta: true, Numero: 3 },
+];
 
 
-if (rtaBoolean === Preguntas.Respuesta) {
-    alert("bien");
-} else {
-    alert("mal"); 
-}
+let NumeroPregunta = 0;
+let Puntaje = 0;
+const elementoPregunta = document.getElementById("pregunta");
+const BtnV = document.getElementById("btnV");
+const BtnF = document.getElementById("btnF");
+const elementoPuntaje = document.getElementById("puntaje");
