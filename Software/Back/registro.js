75d4bd0023c194ad94b8f0b9a230825e9d2f79a7
urzurioliver <urzurioliver@gmail.com>
2025-10-02 18:09:51 -0300

    REGISTRO CON SOQUETIC!!!!!!!

--- Software/Back/registro.js
+++ Software/Back/registro.js
@@ -1,14 +1,15 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-function registro ({rol, nombre, apellido, mail, contraseña}){
-let data = fs.readFileSync ("usuarios.json", "utf-8")
-let usuariosArray = JSON.parse (data)
-let usuario = {rol, nombre, apellido, mail, contraseña}
+function registro (data){
+let usuariosJSON = fs.readFileSync ("usuarios.json", "utf-8")
+let usuariosArray = JSON.parse (usuariosJSON)
+let usuario = data
 usuariosArray.push (usuario)
 let usuariosjson = JSON.stringify (usuariosArray)
 fs.writeFileSync ("usuarios.json", usuariosjson)
+return usuario
 }
 
-subscribePOSTEvent("info", registro)
+subscribePOSTEvent("inforegistro", registro)
 startServer()
