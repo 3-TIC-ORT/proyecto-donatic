4f78764055f4d55d9cff45f7138196e014c4420a
urzurioliver <urzurioliver@gmail.com>
2025-10-04 20:44:17 +0200

    creación de clases mínimamente corregido

--- Software/Back/clases.js
+++ Software/Back/clases.js
@@ -1,15 +1,15 @@
 import fs from "fs"; 
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-function crearClase (data)
+function crearClase (data){
 let clasesJSON = fs.readFileSync ("clases.json", "utf-8")
 let clasesArray = JSON.parse (clasesJSON)
 let clase = data
 clasesArray.push (clase)
 let clasesjson = JSON.stringify (clasesArray)
-fs.writeFileSync ("clases.json", clasejson)
+fs.writeFileSync ("clases.json", clasesjson)
 return clase
 }
 
-subscribePOSTEvent("creacion de clases", crearClase)
+subscribePOSTEvent("creaciondeclases", crearClase)
 startServer()
