eb95bf0816f7dca3eb33ed32e7c4ea7dbd1f5a1f
Nytokon <dante.konoba@gmail.com>
2025-10-06 10:07:51 -0300

    Construyo la interaccion con el ususario en crear clase

--- Software/Front End/Pantalla Crear Clases/CrearClases.js
+++ Software/Front End/Pantalla Crear Clases/CrearClases.js
@@ -3,3 +3,9 @@ function IrClases (){
     window.location.href='../Pantalla Gestionar Clases/Clases.html'
 }
 BotonIrClases.addEventListener("click", (IrClases));
+let Nombre = document.getElementById("nombreClase");
+let Contraseña = document.getElementById("contraseñaClase");
+let BotonCrear = document.getElementById("botonCrear");
+BotonCrear.addEventListener("click",(click) => {
+    alert(Nombre.value +", "+ Contraseña.value);
+  })
