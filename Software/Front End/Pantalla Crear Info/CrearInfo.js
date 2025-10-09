82fc47f398561ebf4bff6474e03fdb6cec8c2460
Nytokon <dante.konoba@gmail.com>
2025-10-09 11:26:40 -0300

    subir info

--- Software/Front End/Pantalla Crear Info/CrearInfo.js
+++ Software/Front End/Pantalla Crear Info/CrearInfo.js
@@ -1,10 +1,39 @@
 let BotonVolver = document.getElementById("VolverAlInicio");
+
 function VolveralInicio (){
     window.location.href='../Pantalla Inicio Educador/Educador.html'
 }
-BotonVolver.addEventListener("click", (VolveralInicio));
+
+BotonVolver.addEventListener("click", VolveralInicio);
+
 let BotonCerrarLaSesion = document.getElementById("ButonCerrarSesion");
+
 function CerrarSesion (){
     window.location.href='../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
 }
-BotonCerrarLaSesion.addEventListener("click", (CerrarSesion));
+
+BotonCerrarLaSesion.addEventListener("click", CerrarSesion);
+
+let ListadeInfo = []
+const inputInfo = document.getElementById("InfoInput")
+const subirInfo = document.getElementById("SubirLaInfo")
+const ulInfo = document.getElementById("Info")
+
+subirInfo.addEventListener("click", function() {
+    
+    const info = inputInfo.value.trim(); 
+    
+    if (info !== "") {
+        ListadeInfo.push(info);
+        inputInfo.value = ""
+
+        ulInfo.innerHTML = ''; 
+
+        for (let i = 0; i < ListadeInfo.length; i++) {
+            const li = document.createElement("li");
+            li.textContent = ListadeInfo[i]
+            li.className = "tipocomentario"
+            ulInfo.appendChild(li);
+        }
+    }
+});
