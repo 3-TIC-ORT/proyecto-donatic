a120116946a14207786eee5796be790b64895a6d
Nytokon <dante.konoba@gmail.com>
2025-10-09 10:25:14 -0300

    Avanzo con las gestion de clases y la muestra de datos en pantalla

--- Software/Front End/Pantalla Gestionar Clases/Clases.js
+++ Software/Front End/Pantalla Gestionar Clases/Clases.js
@@ -8,3 +8,18 @@ function IrCrear (){
     window.location.href='../Pantalla Crear Clases/CrearClases.html'
 }
 BotonIrCrearClases.addEventListener("click", (IrCrear));
+let Clases = [
+    {name: "PrimeroJ", cantidadAlumnos: 19 },
+    {name: "SegundoK", cantidadAlumnos: 24 },
+    {name: "TerceroD", cantidadAlumnos: 12 }
+]
+;//Valen despues hay que hacer que estos datos sean los que vienen de crear clase
+
+const contenedorData = document.getElementById('Nombre');
+
+
+Clases.forEach(clase => {
+    const pClase = document.createElement('p');
+    pClase.textContent = `Curso: ${clase.name} - Alumnos: ${clase.cantidadAlumnos}`;
+    contenedorData.appendChild(pClase);
+});
