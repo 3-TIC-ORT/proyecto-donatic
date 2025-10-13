a55ccb60f9eebc7e098aa1464905ffe9c46fb86e
Nytokon <dante.konoba@gmail.com>
2025-10-13 10:27:20 -0300

    avanzo un poco con juego tipos

--- Software/Front End/Juego Tipos/Tipos.js
+++ Software/Front End/Juego Tipos/Tipos.js
@@ -4,3 +4,14 @@ function IrEstudiante (){
     window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
 }
 BotonEstudiante.addEventListener("click", (IrEstudiante));
+const Donantes = {
+    
+    "O-":  ["O-", "A-", "B-", "AB-", "O+", "A+", "B+", "AB+"], 
+    "O+":  ["O+", "A+", "B+", "AB+"],
+    "A-":  ["A-", "AB-", "A+", "AB+"],
+    "A+":  ["A+", "AB+"],
+    "B-":  ["B-", "AB-", "B+", "AB+"],
+    "B+":  ["B+", "AB+"],
+    "AB-": ["AB-", "AB+"],
+    "AB+": ["AB+"]
+}
