f84a3031ca4a5f43da9091439be5f725b4f9f17f
Nytokon <dante.konoba@gmail.com>
2025-10-13 10:11:33 -0300

    Avanzo con la pantalla de info, y la de juego tipos

---
+++ Software/Front End/Juego Tipos/Tipos.js
@@ -0,0 +1,6 @@
+
+let BotonEstudiante = document.getElementById("Estudiante");
+function IrEstudiante (){
+    window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
+}
+BotonEstudiante.addEventListener("click", (IrEstudiante));
--- Software/Front End/Pantalla Información/Información.js
+++ Software/Front End/Pantalla Información/Información.js
@@ -3,3 +3,18 @@ function VolverAlInicio (){
     window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
 }
 BotonVolver.addEventListener("click", (VolverAlInicio));
+let Infos = [
+    {Infor: "Maxi" },
+    {Infor: "Sanguche" },
+    {Infor: "La sangre se puede donar"}
+]
+;
+
+const contenedorInfo = document.getElementById('Informa');
+
+
+Infos.forEach(Info => {
+    const pInfo = document.createElement('p');
+    pInfo.textContent = `*${Info.Infor}`;
+    contenedorInfo.appendChild(pInfo);
+});
