863d69b4aa7461771aa88bf6411d3f69b80a17b8
Valen <valencampora@gmail.com>
2025-10-14 15:27:21 -0300

    Creo la pantalla para el juego de unir con flechas

---
+++ Software/Front End/Juego unir con flechas/JuegoUnir.js
@@ -0,0 +1,5 @@
+let BotonVolver = document.getElementById("VolverInicio");
+function VolverAlInicio (){
+    window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
+}
+BotonVolver.addEventListener("click", (VolverAlInicio));
--- Software/Front End/Pantalla de Juegos/Selector de Juegos.js
+++ Software/Front End/Pantalla de Juegos/Selector de Juegos.js
@@ -3,13 +3,21 @@ function VolverAlInicio (){
     window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
 }
 BotonVolver.addEventListener("click", (VolverAlInicio));
+
 let BotonJuego1 = document.getElementById("JuegoVF");
 function IrVF (){
     window.location.href='../Juego Verdadero Falso/VF.html'
 }
 BotonJuego1.addEventListener("click", (IrVF));
+
 let BotonJuego2 = document.getElementById("JuegoTipos");
 function IrTipos (){
     window.location.href='../Juego Tipos/Tipos.html'
 }
 BotonJuego2.addEventListener("click", (IrTipos));
+
+let BotonJuego3 = document.getElementById("JuegoUnir");
+function IrJuegoUnir (){
+    window.location.href='../Juego unir con flechas/JuegoUnir.html'
+}
+BotonJuego3.addEventListener("click",(IrJuegoUnir));
