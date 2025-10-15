5a103ba0ff9af180992aa115ffa5a25dff72dc54
urzurioliver <urzurioliver@gmail.com>
2025-10-15 19:29:31 +0200

    guardar los resultados de los juegos de cada alumno

--- Software/Back/juegos.js
+++ Software/Back/juegos.js
@@ -1,1 +1,15 @@
 import fs from "fs"
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+
+function juegos (data){
+let juegosJSON = fs.readFileSync ("juegos.json", "utf-8")
+let juegosArray = JSON.parse (juegosJSON)
+let juegosResultado = data
+juegosArray.push (juesgosResultado)
+let juegosjson = JSON.stringify (juegosArray)
+fs.writeFileSync ("juegos.json", juegosjson)
+return juegosResultado
+}
+
+subscribePOSTEvent("juegosResultados", juegos)
+startServer()
