5138c076c85b11f5ab363c7f74f0a00f0a40f4fe
Valen <valencampora@gmail.com>
2025-10-16 14:09:24 -0300

    agrego el logo a todas las pantallas

--- Software/Front End/Carpeta de seleccionar rol/Pantalla seleccionar rol.js
+++
@@ -1,5 +0,0 @@
-let BotonVolver= document.getElementById("BotonVolver")
-BotonVolver.addEventListener("click", Funcionvolver)
-function Funcionvolver (){
-    window.location.href = '../Pantalla de inicio no registrados/pantalla de inicio no registrados.html'
-}
