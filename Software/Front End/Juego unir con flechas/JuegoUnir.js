d8bb2b9581e5bc8116c0982206e75c4dcb0a3cb9
Valen <valencampora@gmail.com>
2025-10-21 08:34:02 -0300

    sigo con el juego

--- Software/Front End/Juego unir con flechas/JuegoUnir.js
+++ Software/Front End/Juego unir con flechas/JuegoUnir.js
@@ -1,4 +1,4 @@
-let BotonVolver = document.getElementById("VolverInicio");
+let BotonVolver = document.getElementById("BotonVolver");
 function VolverAlInicio (){
     window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
 }
