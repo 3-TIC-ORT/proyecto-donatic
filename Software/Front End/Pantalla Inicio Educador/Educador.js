80ad9e07b779a18f23f20cd0fd6cf4a63059250d
Valen <valencampora@gmail.com>
2025-10-28 14:26:36 -0300

    cambio pantalla inicio educador

--- Software/Front End/Pantalla Inicio Educador/Educador.js
+++ Software/Front End/Pantalla Inicio Educador/Educador.js
@@ -1,25 +1,29 @@
-    let BotonIrCrearInfo = document.getElementById("BotonIrInfo");
+let BotonGestionar = document.getElementById("BotonGestionarPerfil");
+function Perfil(){
+    window.location.href='../Pantalla de gestion de perfil educador/GestionPerfilEdu.html'
+}
+BotonGestionar.addEventListener("click", (Perfil))
+
+let BotonIrCrearInfo = document.getElementById("BotonIrInfo");
 function IrCrearInfo (){
     window.location.href='../Pantalla Crear Info/CrearInfo.html'
 }
 BotonIrCrearInfo.addEventListener("click", (IrCrearInfo));
+
     let BotonCerrarLaSesion = document.getElementById("BotonCerrarSesion");
 function CerrarSesion (){
     window.location.href='../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
 }
 BotonCerrarLaSesion.addEventListener("click", (CerrarSesion));
+
     let BotonIrClases = document.getElementById("BotonClases");
 function IrClases (){
     window.location.href='../Pantalla Gestionar Clases/Clases.html'
 }
 BotonIrClases.addEventListener("click", (IrClases));
+
     let BotonIrStats = document.getElementById("BotonIrEstadisticas");
 function IrStats (){
     window.location.href='../Pantalla Estadisticas/Stats.html'
 }
 BotonIrStats.addEventListener("click", (IrStats));
-let BotonGestionar = document.getElementById('BotonGestionarPerfil');
-function Perfil(){
-    window.location.href='../Pantalla de gestion de perfil educador/GestionPerfilEdu.html'
-}
-BotonGestionar.addEventListener("click", (Perfil))
--- Software/Front End/Pantalla de gestion de perfil educador/GestionPerfilEdu.js
+++ Software/Front End/Pantalla de gestion de perfil educador/GestionPerfilEdu.js
@@ -1,8 +1,9 @@
 let BotonVolver = document.getElementById("VolverInicio");
 function VolverAlInicio (){
-    window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
+    window.location.href='../Pantalla de gestion de perfil educador/GestionPerfilEdu.html'
 }
 BotonVolver.addEventListener("click", (VolverAlInicio));
+
 let BotonCerrarSesion = document.getElementById("BotonCerrarSesion");
 function FuncionCerrarSesion(){
     window.location.href = '../Pantalla de inicio no registrados/pantalla de inicio no registrados.html'
