b3b79d377727017620c9f8b396ca71bf6d3f688c
dante <dante.konoba@gmail.com>
2025-10-30 10:34:12 -0300

    soquetic

--- Software/Front End/Pantalla de registro/PantallaRegistro.js
+++ Software/Front End/Pantalla de registro/PantallaRegistro.js
@@ -1,8 +1,36 @@
+connect2Server();
+const Botonvolver = document.getElementById("Botonvolver");
+const BotonRegistrarse = document.getElementById("BotonRegistrarse");
+const inputNombreYApellido = document.getElementById("inputNombre");
+const inputContrasena = document.getElementById("inputContrasena");
+const inputCodigo = document.getElementById("inputCodigoClase");
+const inputFileFoto = document.getElementById("inputFotoPerfil");
 Botonvolver.addEventListener("click", Funcionvolver)
 function Funcionvolver (){
     window.location.href = '../Pantalla de inicio no registrados/pantalla de inicio no registrados.html'
 }
-BotonRegistrarse.addEventListener("click", FuncionRegistrarse)
-function FuncionRegistrarse (){
-    window.location.href = '../Pantalla Inicio Educador/Educador.html'
-}
+BotonRegistrarse.addEventListener("click", () => {
+    const nombre = inputNombreYApellido.value;
+    const contrasena = inputContrasena.value;
+    const codigo = inputCodigo.value;
+    const fotoDePerfil = inputFileFoto.files[0]; 
+    const datosRegistro = {
+        nombreCompleto: nombre,
+        clave: contrasena,
+        codigoClase: codigo,
+        foto: fotoDePerfil
+    };
+
+
+    postEvent("REGISTRO", datosRegistro, (respuesta) => {
+        console.log("Respuesta del Backend:", respuesta);
+        if (respuesta && respuestaDelBackend.true) {
+            window.location.href = '../Pantalla Inicio Educador/Educador.html';
+        } else {
+            alert("Error en el registro: " + (respuestaDelBackend ? respuestaDelBackend.mensaje : "El servidor no respondió."));
+        }
+    });
+});
+
+
+
