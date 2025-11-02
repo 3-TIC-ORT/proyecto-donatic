4045853cc5fbeafa10a580cc4214207343487664
Valen <valencampora@gmail.com>
2025-11-02 14:26:41 -0300

    Verdadero o falso

--- Software/Front End/Juego Verdadero Falso/VF.js
+++ Software/Front End/Juego Verdadero Falso/VF.js
@@ -1,9 +1,15 @@
 const Preguntas = [
     { pregunta: "LAS DONACIONES DE SANGRE PUEDEN DEBILITAR AL CUERPO PERMANENTEMENTE", Respuesta: false, Numero: 1 },
-    { pregunta: "LA SANGRE DONADA SE PUEDE DIVIDIR EN COMPONENTES", Respuesta: true, Numero: 2 },
+    { pregunta: "AL DONAR SANGRE, ME PUEDO CONTAGIAR DE ALGUNA ENFERMEDAD", Respuesta: false, Numero: 2 },
     { pregunta: "AL TOMAR MEDICAMENTOS, NO TENGO LA POSIBILIDAD DE DONAR SANGRE", Respuesta: false, Numero: 3 },
-    { pregunta: "DESPUÉS DE DONAR UNA VEZ, NO PUEDO DONAR NUNCA MÁS", Respuesta: false, Numero: 3 },
-    { pregunta: "GRACIAS A LOS EXAMENES PREVIOS A LA DONACIÓN, SE PUEDE DETECTAR ALGUNA ENFERMEDAD", Respuesta: true, Numero: 2 },
+    { pregunta: "SI A LO LARGO DE MI VIDA DONO MUCHAS VECES, VOY A DESARROLLAR ANEMIA", Respuesta: false, Numero: 4 },
+    { pregunta: "DESPUES DE DONAR, NO PUEDO HACER NINGUN TIPO DE EJERCICIO", Respuesta: true, Numero: 5 },
+    { pregunta: "DESPUÉS DE DONAR UNA VEZ, NO PUEDO DONAR NUNCA MÁS", Respuesta: false, Numero: 6 },
+    { pregunta: "UNA UNICA DONACIÓN PUEDE AYUDAR A SALVAR MULTIPLES VIDAS", Respuesta: true, Numero: 7 },
+    { pregunta: "GRACIAS A LOS EXAMENES PREVIOS A LA DONACIÓN, SE PUEDE DETECTAR ALGUNA ENFERMEDAD", Respuesta: true, Numero: 8 },
+    { pregunta: "LA SANGRE DONADA SE PUEDE DIVIDIR EN COMPONENTES", Respuesta: true, Numero: 9 },
+    { pregunta: "CIERTOS TIPOS DE SANGRE SON MÁS NECESITADOS EN LOS HOSPITALES POR SU ESCASEZ", Respuesta: true, Numero: 10 },
+
 ];
 
 
