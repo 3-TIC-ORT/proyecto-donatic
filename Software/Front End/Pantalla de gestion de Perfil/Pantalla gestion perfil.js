709e4c5639bcaa0b723ccfeabcd6b49061db617a
Valen <valencampora@gmail.com>
2025-11-03 18:52:24 -0300

    termino css selector de juegos y gestion de perfil alumno

--- Software/Front End/Pantalla de gestion de Perfil/Pantalla gestion perfil.js
+++ Software/Front End/Pantalla de gestion de Perfil/Pantalla gestion perfil.js
@@ -8,3 +8,14 @@ function FuncionCerrarSesion(){
     window.location.href = '../Pantalla de inicio no registrados/pantalla de inicio no registrados.html'
 };
 BotonCerrarSesion.addEventListener("click",(FuncionCerrarSesion));
+
+function moveNext(current, nextId) {
+    if (current.value.length === 1 && nextId) {
+      document.getElementById(nextId).focus();
+    }
+  }
+  function moveBack(event, prevId) {
+    if (event.key === "Backspace" && !event.target.value && prevId) {
+      document.getElementById(prevId).focus();
+    }
+  }
