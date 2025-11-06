9525551332cd04bdd0b3de7e02ba0bf24af65d74
dante <dante.konoba@gmail.com>
2025-11-06 10:26:43 -0300

    debugeo boton valen, en el commit anterior arregle codigo de clase

--- Software/Front End/Pantalla Crear Info/CrearInfo.js
+++ Software/Front End/Pantalla Crear Info/CrearInfo.js
@@ -1,39 +1,33 @@
+connect2Server();
 let BotonVolver = document.getElementById("VolverInicio");
 
-function VolveralInicio (){
-    window.location.href='../Pantalla Inicio Educador/Educador.html'
+function VolveralInicio() {
+  window.location.href = "../Pantalla Inicio Educador/Educador.html";
 }
 
 BotonVolver.addEventListener("click", VolveralInicio);
 
 let BotonCerrarLaSesion = document.getElementById("ButonCerrarSesion");
 
-function CerrarSesion (){
-    window.location.href='../pantalla de inicio no registrados/pantalla de inicio no registrados.html'
-}
+let ListadeInfo = [];
+const inputInfo = document.getElementById("InfoInput");
+const subirInfo = document.getElementById("SubirLaInfo");
+const ulInfo = document.getElementById("Info");
 
-BotonCerrarLaSesion.addEventListener("click", CerrarSesion);
+subirInfo.addEventListener("click", function () {
+  const info = inputInfo.value.trim();
 
-let ListadeInfo = []
-const inputInfo = document.getElementById("InfoInput")
-const subirInfo = document.getElementById("SubirLaInfo")
-const ulInfo = document.getElementById("Info")
+  if (info !== "") {
+    ListadeInfo.push(info);
+    inputInfo.value = "";
 
-subirInfo.addEventListener("click", function() {
-    
-    const info = inputInfo.value.trim(); 
-    
-    if (info !== "") {
-        ListadeInfo.push(info);
-        inputInfo.value = ""
+    ulInfo.innerHTML = "";
 
-        ulInfo.innerHTML = ''; 
-
-        for (let i = 0; i < ListadeInfo.length; i++) {
-            const li = document.createElement("li");
-            li.textContent = ListadeInfo[i]
-            li.className = "tipocomentario"
-            ulInfo.appendChild(li);
-        }
+    for (let i = 0; i < ListadeInfo.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = ListadeInfo[i];
+      li.className = "tipocomentario";
+      ulInfo.appendChild(li);
     }
+  }
 });
