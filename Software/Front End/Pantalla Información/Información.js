9cc83ba963d581995e307333994e2b44cf965027
dante <dante.konoba@gmail.com>
2025-11-06 12:05:25 -0300

    arreglar mostrar info

--- Software/Front End/Pantalla Información/Información.js
+++ Software/Front End/Pantalla Información/Información.js
@@ -1,20 +1,17 @@
-let BotonVolver = document.getElementById("VolverInicio");
 function VolverAlInicio (){
     window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
 }
-BotonVolver.addEventListener("click", (VolverAlInicio));
-let Infos = [
-    {Infor: "Maxi" },
-    {Infor: "Sanguche" },
-    {Infor: "La sangre se puede donar"}
-]
-;
+
+BotonVolver.addEventListener("click", VolverAlInicio);
 
 const contenedorInfo = document.getElementById('Informa');
 
-
-Infos.forEach(Info => {
-    const pInfo = document.createElement('p');
-    pInfo.textContent = `*${Info.Infor}`;
-    contenedorInfo.appendChild(pInfo);
+getEvent("mostrarinfo", (comentariosRecibidos) => {
+    if (Array.isArray(comentariosRecibidos)) {
+        comentariosRecibidos.forEach(Info => {
+            const pInfo = document.createElement('p');
+            pInfo.textContent = `*${Info.Infor}`;
+            contenedorInfo.appendChild(pInfo);
+        });
+    }
 });
