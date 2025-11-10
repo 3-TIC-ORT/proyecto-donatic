14f3fce1ddf2429634cdf9cea679107af4636415
urzuri <urzurioliver@gmail.com>
2025-11-10 15:23:51 -0300

    arregle lo de mostrar  info

--- Software/Front End/Pantalla Información/Información.js
+++ Software/Front End/Pantalla Información/Información.js
@@ -1,19 +1,20 @@
-connect2Server()
+connect2Server();
 const BotonVolver = document.getElementById("VolverInicio");
-function VolverAlInicio (){
-    window.location.href='../Pantalla de inicio estudiantes/Inicio estudiantes.html'
+function VolverAlInicio() {
+  window.location.href =
+    "../Pantalla de inicio estudiantes/Inicio estudiantes.html";
 }
 
 BotonVolver.addEventListener("click", VolverAlInicio);
 
-const contenedorInfo = document.getElementById('Informa');
+const contenedorInfo = document.getElementById("Informa");
 
 getEvent("mostrarinfo", (comentariosRecibidos) => {
-    if (Array.isArray(comentariosRecibidos)) {
-        comentariosRecibidos.forEach(Info => {
-            const pInfo = document.createElement('p');
-            pInfo.textContent = `*${Info.Infor}`;
-            contenedorInfo.appendChild(pInfo);
-        });
-    }
+  if (Array.isArray(comentariosRecibidos)) {
+    comentariosRecibidos.forEach((Info) => {
+      const pInfo = document.createElement("p");
+      pInfo.textContent = `user: ${Info.post}`;
+      contenedorInfo.appendChild(pInfo);
+    });
+  }
 });
